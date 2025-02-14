74980ee7ee731892391b642c7b1037818433e8cb
termisam005 <termi0004@gmail.com>
2025-02-14 18:55:14 +0900

    first delete

--- src/components/common/ButtonComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ButtonComponent = ({ type, id, className, onClick, txt }) => {
-  return (
-    <button type={type} id={id} className={className} onClick={onClick}>{txt}</button>
-  );
-};
-
-export default ButtonComponent;
